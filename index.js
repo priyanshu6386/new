d074a46cc99be467ac4ba854d9110a740f6a621a
priyanshu6386 <priyanshuv624@gmail.com>
2022-10-15 13:13:34 +0530

    commit message

---
+++ index.js
@@ -0,0 +1,33 @@
+const signUp= () =>
+{
+document.getElementById('home').style.display='none'
+document.getElementById('sign-up').style.display='block'
+document.getElementById('sign-in').style.display='none'
+
+
+}
+
+const home = () =>
+{
+document.getElementById('home').style.display='block'
+document.getElementById('sign-up').style.display='none'
+
+document.getElementById('sign-in').style.display='none'
+
+}
+
+const signIn = () =>
+{
+document.getElementById('home').style.display='none'
+document.getElementById('sign-in').style.display='block'
+document.getElementById('sign-up').style.display='none'
+
+}
+
+const hom = () =>
+{
+document.getElementById('home').style.display='block'
+document.getElementById('sign-in').style.display='none'
+document.getElementById('sign-up').style.display='none'
+
+}
